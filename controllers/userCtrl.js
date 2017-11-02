a1dac25ad79fd4694356f4943e447091a604a413
An-AngryBear <monahajt@gmail.com>
2017-11-02 15:05:29 -0500

    add button functionality and edit functionality to profile page

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,5 +1,70 @@
 'use strict'
 
-module.exports.postAvatar = () => {
-    
+//get user info for display in profile
+module.exports.getUserInfo = (req, res, next) => {
+    console.log("get user info")
+    const { User } = req.app.get('models');
+    User.findById(req.params.id, { raw: true })
+    .then( (data) => {
+        let birthdate = dateConverter(data.birthdate);
+        let timezone = timezoneTextParser(data.timezone);
+        let loggedUser;
+        req.params.id == req.session.passport.user.id ? loggedUser = true : loggedUser = false
+        res.render('profile', {
+            screen_name: data.screen_name,
+            email: data.email,
+            birthdate,
+            language: data.language,
+            timezone,
+            avatar: data.avatar,
+            blurb: data.blurb ? data.blurb : null,
+            loggedUser
+        })
+    });
+}
+
+module.exports.editUserInfo = (req, res, next) => {
+    console.log("edit user info")
+    const { User } = req.app.get('models');
+    User.findById(req.session.passport.user.id, {raw: true})
+    .then( (data) => {
+        console.log("data", data)
+        return User.update({
+        screen_name: req.body.screen_name ? req.body.screen_name : data.screen_name,
+        email: req.body.email ? req.body.email : data.email,
+        password: data.password,
+        birthdate: data.birthdate,
+        language: req.body.language ? req.body.language : data.language,
+        timezone: req.body.timezone ? req.body.timezone : data.timezone,
+        avatar: req.body.avatar ? req.body.avatar : data.avatar,
+        last_logged_in: data.last_logged_in,
+        blurb: req.body.blurb ? req.body.blurb : data.blurb,
+        }, {where: {id: req.session.passport.user.id}})
+    })
+    .then( (data1) => {
+        console.log("data1", data1)
+        res.redirect(`/user/${req.session.passport.user.id}`)
+    });
+}
+// buyer_id:req.session.passport.user.id,
+// payment_id: req.body.payment_id,
+// order_date: req.body.order_date,
+// createdAt:null,
+// updatedAt:null
+// }, {where:{id: req.params.id}})
+
+
+//converts long date to xx/xx/xxxx format
+let dateConverter = (date) => {
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let newDate = `${month}/${day}/${year}`;
+    return newDate;
+}
+
+//removes the -xx:xx or +xx:xx from the timezone string, leaving just the zone-title
+let timezoneTextParser = (timezoneString) => {
+    let timezoneArr = timezoneString.split(/[0-9]+/);
+    return timezoneArr[2];
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,19 @@
 const { Router } = require('express');
 const router = Router();
 
-const { postAvatar } = ('../controllers/userCtrl')
+const { getUserInfo, editUserInfo } = require('../controllers/userCtrl')
 
-router.patch('/user/avatar', postAvatar)
+router.put('/user', isLoggedIn, editUserInfo, getUserInfo)
+router.get('/user/:id', isLoggedIn, getUserInfo)
+// router.put('user/email', isLoggedIn)
+// router.put('user/language', isLoggedIn)
+// router.put('user/timezone', isLoggedIn)
+
+function isLoggedIn(req, res, next) {
+    console.log("isLoggedIn?")
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/login');
+}
+
 module.exports = router;

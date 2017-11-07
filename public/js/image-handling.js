9c3ae30ffd3c78c82061e1d13be7ce62052d7f6e
An-AngryBear <monahajt@gmail.com>
2017-11-07 15:22:21 -0600

    basic styling, add images, add avatars

---
+++ public/js/image-handling.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// done as ifs for speed of image load
+
+if(window.location.pathname === '/login') {
+    $('#body').css('background-image', 'url(/public/imgs/ds4.jpeg')
+}
+
+if(window.location.pathname.indexOf('/user/') >= 0) {
+    $('#body').css('background-image', 'url(/public/imgs/keyboard.jpg')
+}
+
+// if(window.location.pathname === '/login') {
+//     $('#body').css('background-image', 'url(/public/imgs/ds4.jpeg')
+// }
+
+// if(window.location.pathname === '/login') {
+//     $('#body').css('background-image', 'url(/public/imgs/ds4.jpeg')
+// }
+
+// if(window.location.pathname === '/login') {
+//     $('#body').css('background-image', 'url(/public/imgs/ds4.jpeg')
+// }

43925441be99104705ecc1258afd29fcde0e728a
An-AngryBear <monahajt@gmail.com>
2018-01-07 14:04:58 -0600

    add live message functionality with socket io

---
+++ public/js/socketio.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const socket = io.connect();
+
+//add user
+console.log(userID);
+
+if(!userID) {
+    var userID = $('#body').attr('data');
+    socket.emit("add-user", {"userID": userID});
+}
+
+$('#send-msg').click( function() {
+    socket.emit("private-message", {
+        "userID": parseInt($(this).attr('data')),
+        "content": $('#msg-content').val()
+    });
+});
+
+socket.on("add-message", function(data){
+    console.log("TRIGGER ADD MSG")
+    $('.msg-box').append(`<li class="user-message other-user-msg">${data.content}</li><br>`);
+});
+
+// `<li class="user-message current-user-msg">${msgContent}</li><br>`

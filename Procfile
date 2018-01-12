37ffcfc4d62f249621c79a67def0b47b41d39cd6
An-AngryBear <monahajt@gmail.com>
2018-01-12 11:45:50 -0600

    create simple script that build config.json file with env variables

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+web: npm run build-config
 web: npm start
---
+++ buildjson.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+require('dotenv').config();
+
+let config = {
+    "development": {
+        "database": "down2game",
+        "host": "127.0.0.1",
+        "dialect": "postgres",
+        "username": "postgres",
+        "password": process.env.DEV_PASS
+      },
+      "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "postgres"
+      },
+      "production": {
+        "username": process.env.PROD_USER,
+        "password": process.env.PROD_PASS,
+        "database": process.env.DATABASE,
+        "host": "ec2-23-23-150-141.compute-1.amazonaws.com",
+        "dialect": "postgres"
+    }
+};
+
+let json = JSON.stringify(config);
+fs.writeFileSync('config/config.json', json, 'utf8');
+console.log(config);
